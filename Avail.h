4e65b94a2a4f176d4bd2899dfb02e423fd9f25c0
Michael Keyser <Erutan409@users.noreply.github.com>
2017-12-23 17:35:57 -0500

    Added `micros()` support

--- Avail.h
+++ Avail.h
@@ -3,18 +3,38 @@
 
 #include <Arduino.h>
 
-bool avail(unsigned long *margin, unsigned long *lastMillis, bool &rollover) {
-	unsigned long milliseconds = millis();
-	unsigned long diff = milliseconds - *lastMillis;
-	rollover = diff > milliseconds;
-	if (rollover) { diff = 4294967295 - *lastMillis + milliseconds; }
+class Avail {
 
-	return *margin <= diff;
-}
+	public:
 
-bool avail(unsigned long *margin, unsigned long *lastMillis) {
-	bool _;
-	return avail(margin, lastMillis, _);
-}
+		static bool micros(unsigned long *margin, unsigned long *lastMicros, bool &rollover) {
+			return Avail::_avail(::micros(), margin, lastMicros, rollover);
+		};
+
+		static bool micros(unsigned long *margin, unsigned long *lastMicros) {
+			bool _;
+			return Avail::micros(margin, lastMicros, _);
+		};
+
+		static bool millis(unsigned long *margin, unsigned long *lastMillis, bool &rollover) {
+			return Avail::_avail(::millis(), margin, lastMillis, rollover);
+		};
+
+		static bool millis(unsigned long *margin, unsigned long *lastMillis) {
+			bool _;
+			return Avail::millis(margin, lastMillis, _);
+		};
+
+	private:
+	
+		static bool _avail(unsigned long metric, unsigned long *margin, unsigned long *lastLong, bool &rollover) {
+			unsigned long diff = metric - *lastLong;
+			rollover = diff > metric;
+			if (rollover) { diff = 4294967295 - *lastLong + metric; }
+
+			return *margin <= diff;
+		};
+
+};
 
 #endif
